e8334c6f60ce58df8c7585f20ceb0bee10b70886
ap-sree <13144620+ap-sree@users.noreply.github.com>
2026-01-29 01:37:51 +0530

    chore: remove backend and electron directories

--- backend/inspect_users.js
+++
@@ -1,8 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./app.db');
-
-db.all("PRAGMA table_info(users)", [], (err, columns) => {
-    if (err) console.error(err);
-    else console.log("Users Schema:", columns);
-    db.close();
-});
--- electron/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    sendNotification: (title, body) => ipcRenderer.send('show-notification', { title, body }),
-    onNotification: (callback) => ipcRenderer.on('on-notification', (_event, value) => callback(value))
-});
